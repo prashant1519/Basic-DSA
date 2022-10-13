fa35a8ebece3101c39fa924f9a4a9b3007176fc0
prashant1519 <prashantsingh19991215@gmail.com>
2022-10-13 11:32:34 +0530

    days weekl

---
+++ Week day.js
@@ -0,0 +1,11 @@
+var arr=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+var n=8
+var day="Wednesday"
+for(var i=0;i<arr.length;i++){
+    if(arr[i]==day){
+        var a=(i+n)%7
+    }
+ 
+}
+console.log(arr[a])
