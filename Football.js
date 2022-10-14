c1adef35aaf938af14e26448fbd34f2c9f13f874
prashant1519 <prashantsingh19991215@gmail.com>
2022-10-14 13:48:24 +0530

    Team

---
+++ Football.js
@@ -0,0 +1,22 @@
+var arr=["A","B","A","B","A","B","B"]
+var bag=""
+var obj={}
+var max=-Infinity;
+for(var i=0;i<arr.length;i++){
+    var str=arr[i]
+    if(obj[str]==undefined){
+     obj[str]=1;
+    }
+    else{
+        obj[str]+=1
+    }
+}
+// console.log(obj)
+for(var key in obj){
+if(obj[key]>max){
+    max=obj[key]
+    bag=key
+}
+       
+}
+console.log(bag)
