0e441e4a3412d986350633a97573ae697d666c28
prashant1519 <prashantsingh19991215@gmail.com>
2022-10-15 21:12:49 +0530

    one sum

---
+++ two arr 1sum.js
@@ -0,0 +1,11 @@
+var arr=[1,2,3,4,5]
+var arr1=[6,7,8,9,10]
+ var sum=0;
+ var sum1=0;
+for(var i=0;i<arr.length;i++){
+sum=sum+arr[i];
+}
+for(var i=0;i<arr.length;i++){
+    sum1=sum1+arr1[i];
+    }
+    console.log(sum+sum1);
