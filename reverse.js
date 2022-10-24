db5578ee863b7f3a875d501993c85e466c0e1e1b
prashant1519 <prashantsingh19991215@gmail.com>
2022-10-24 16:29:31 +0530

    reverse

---
+++ reverse.js
@@ -0,0 +1,17 @@
+var arr=[12,34,11,9,18];
+var left=0;
+var right=arr.length-1;
+
+
+function swap(arr,right,left){
+    let temp=arr[right]
+    arr[right]=arr[left]
+    arr[left]=temp
+}
+
+while(left<right){
+     swap(arr,left,right)
+     left++;
+     right--;
+}
+console.log(arr)
