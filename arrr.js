4d424b7f1e0f8a632f624a52cd3ff896fc5dd837
prashant1519 <prashantsingh19991215@gmail.com>
2022-11-01 08:43:26 +0530

    arr

--- arrr.js
+++
@@ -1,7 +0,0 @@
-
-var arr=[1,2,34,4]
-for(var i=0;i<arr.length;i++){
-  
-}
-  console.log(arr);
-
---
+++ missing int.js
@@ -0,0 +1,10 @@
+let arr=[1,2,3,5,6]
+
+for(var i=0;i<arr.length;i++){
+  if(arr[i]!==i+1){
+    console.log(i+1);
+    break;
+  }
+
+}
+
