df8ef23baf6611c8f9ff7bb5c46cce404d77c964
prashant1519 <prashantsingh19991215@gmail.com>
2022-11-02 21:31:36 +0530

    arr

--- differene.js
+++ differene.js
@@ -1,19 +1,19 @@
 // burte force
 
-// var arr=[1,2,3,4,5]
-// var k=8
-// var fag="no";
+var arr=[1,2,3,4,5]
+var k=8
+var fag="no";
 
-// for(var i=0;i<arr.length;i++){
-//     for(var j=i+1;j<arr.length;j++){
-//         if(arr[j]-arr[i]===k){
-//           fag="yes"
-//         }
+for(var i=0;i<arr.length;i++){
+    for(var j=i+1;j<arr.length;j++){
+        if(arr[j]-arr[i]===k){
+          fag="yes"
+        }
         
 
-//     }
-// }
-// console.log(fag)
+    }
+}
+console.log(fag)
 
 
 // time complexity
