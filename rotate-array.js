6d251b0f3769df7e4a0b1dc175b633ce1233671d
prashant1519 <prashantsingh19991215@gmail.com>
2022-11-07 12:13:04 +0530

    Rotate array

---
+++ rotate-array.js
@@ -0,0 +1,45 @@
+let arr=[1,2,3,4]
+let left=0
+let right=arr.length-1;
+let k=7;
+let t=k%arr.length
+
+function rotate(arr,left,right){
+    // while(left<right){
+    
+
+    
+            reverse(arr,left,right)
+          
+        
+            swap(arr,0,t-1)
+
+            swap(arr,t,arr.length-1)
+        
+
+    // }
+    
+}
+rotate(arr,left,right)
+console.log(arr);
+
+
+
+function reverse(arr,left,right){
+    while(left<right){
+
+        swap(arr,left,right)
+        left++;
+        right--;
+    }
+    
+}
+
+function swap(arr,left,right){
+    let temp=arr[left]
+    arr[left]=arr[right]
+    arr[right]=temp
+
+}
+
+
